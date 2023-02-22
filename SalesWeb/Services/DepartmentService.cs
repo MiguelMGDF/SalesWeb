ade4dd876dfd3c693d6646b7bce3e8bc7431fa83
Miguel Cortez <miguelcortezrj@gmail.com>
2023-02-22 17:49:13 -0300

    Added SellerFormViewModel and Department select component

--- SalesWeb/Services/DepartmentService.cs
+++ SalesWeb/Services/DepartmentService.cs
@@ -7,24 +7,18 @@ using System.Threading.Tasks;
 
 namespace SalesWeb.Services
 {
-    public class SellerService
+    public class DepartmentService
     {
         private readonly SalesWebContext _context;
 
-        public SellerService(SalesWebContext context)
+        public DepartmentService(SalesWebContext context)
         {
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public List<Department> FindAll()
         {
-            return _context.Seller.ToList();
-        }
-
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
+            return _context.Department.OrderBy(x => x.Name).ToList();
         }
     }
 }
